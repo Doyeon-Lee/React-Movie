0e7800a6993f40eb7daa1112263230781c946a4b
Doyeon-Lee <dyl1229@naver.com>
2021-01-14 22:22:29 +0900

    MAKE movie COMPONENT AND SHOW movie DATA

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component{
   state = {
@@ -11,16 +12,45 @@ class App extends React.Component{
   getMovies = async() => {
     //As axios takes some time to be finished, we have to 
     //tell componentDidMount to wait until the task is done
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {
+      data: {
+        data : {movies}
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    
+    //It is movies: movies, but, JS is smart and can understand the following
+    this.setState({movies, isLoading: false});
   }
 
   componentDidMount(){
     this.getMovies();
+
   }
 
   render(){
-    const {isLoading} = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>
+    const {isLoading, movies} = this.state;
+    return (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map(movie => (
+              <Movie 
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+          </div>
+        )} 
+      </section>   
+    );
   }
 }
 
