acda84275a839e503f86b93bdaf1f72137b0e483
Doyeon-Lee <dyl1229@naver.com>
2021-01-15 16:37:36 +0900

    ADD genre LSIT

--- src/Movie.js
+++ src/Movie.js
@@ -5,13 +5,18 @@ import "./Movie.css";
 //Movie component doesn't need state, and if the state is 
 //unnecessary, it doesn't have to be class component
 
-function Movie({year, title, summary, poster}){
+function Movie({year, title, summary, poster, genres}){
     return(
         <div class="movie">
             <img src={poster} alt={title} title={title}/>
             <div class="movie__data">
                 <h3 class="movie__title">{title}</h3>
                 <h5 class="movie__year">{year}</h5>
+                <ul class="genres">
+                    {genres.map((genre, index) => (
+                        <li key={index} class="genres__genre">{genre}</li>
+                    ))}
+                </ul>
                 <p class="movie__summary">{summary}</p>
             </div>
         </div>
@@ -23,7 +28,8 @@ Movie.propTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export default Movie;
